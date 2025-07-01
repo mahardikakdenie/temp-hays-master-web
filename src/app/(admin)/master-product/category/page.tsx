f764b99b7c4226bda33414a97a09d02cd8c002cb
mahardikakdenie <kucingemas744@gmail.com>
2025-07-01 23:17:37 +0700

    feat(master-product): initial route page for this feature

---
+++ src/app/(admin)/master-product/category/page.tsx
@@ -0,0 +1,9 @@
+const categoryPage = () => {
+  return (
+    <div>
+      <h1>Category Page</h1>
+      <p>This is the category page content.</p>
+    </div>
+  );
+};
+export default categoryPage;
