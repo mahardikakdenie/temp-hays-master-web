4d58c27e8dfa5d641607c7f6fa0f343938e80c96
mahardikakdenie <kucingemas744@gmail.com>
2025-07-11 17:57:28 +0700

    refactor contracts making DATA UI components

--- src/views/master-setup/contact/components/ContactTable.tsx
+++ src/views/master-setup/contact/components/ContactTable.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableHeader, TableRow } from '@/components/ui/table/Table';
+import { Table } from '@/components/ui/table/Table';
 import SearchIcon from '@/components/icons/Search';
 import ButtonSecondary from '@/components/ui/button/ButtonSecondary';
 import Input from '@/components/ui/form/Input';
-import { cn } from '@/libs/utils/cn.utils';
 import useContact from '../contact.hook';
-import PencilSquareIcon from '@/components/icons/PencilSquare';
 import Pagination from '@/components/ui/table/Pagination';
+import HeaderDataUI from '@/components/ui/table/HeaderData';
+import TableDataUI from '@/components/ui/table/TableData';
 
 const ContactTable: React.FC = () => {
   const {
@@ -14,33 +14,41 @@ const ContactTable: React.FC = () => {
     isFetching,
     data: contacts,
     error,
-    // sort,
+    sort,
     meta,
     onMeta,
     // setMeta,
     // onSearch,
-    // onSort,
+    onSort,
     onRetry,
   } = useContact();
   const onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log('Search:', event.target.value);
   };
 
-  const tableStatus =
-    isLoading || (error && isFetching) ? (
-      'Loading...'
-    ) : error ? (
-      <div className="flex items-center gap-2">
-        <span>{error}</span>
-        <button onClick={onRetry} className="text-red-500 underline">
-          Try again
-        </button>
-      </div>
-    ) : contacts.length === 0 ? (
-      'No Contact found.'
-    ) : null;
-
-  const headers = ['Name', 'Email', 'Phone', 'WhatsApp Phone', 'Status', 'Actions'];
+  const headers = [
+    { name: 'Name', key: 'name' },
+    {
+      name: 'Email',
+      key: 'email',
+    },
+    {
+      name: 'Phone',
+      key: 'phone',
+    },
+    {
+      name: 'WhatsApp Phone',
+      key: 'wa_phone',
+    },
+    {
+      name: 'Status',
+      key: 'status',
+    },
+    {
+      name: 'Actions',
+      key: 'actions',
+    },
+  ];
   return (
     <div className="widget-dark p-6 flex flex-col gap-4">
       <div className="flex flex-col md:flex-row justify-between md:items-center">
@@ -62,69 +70,23 @@ const ContactTable: React.FC = () => {
       </div>
 
       <Table>
-        <TableHeader>
-          <TableRow>
-            {headers.map((header, index) => (
-              <TableCell
-                key={index}
-                isHeader
-                className="text-center font-semibold"
-                sortable={true}
-                sortKey={header.toLowerCase()}
-              >
-                {header}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHeader>
+        <HeaderDataUI
+          headerWithSorts={headers
+            .filter((header) => header.key !== 'actions')
+            .map((header) => header.key)}
+          onSort={onSort}
+          sort={sort}
+          headers={headers}
+        />
 
-        <TableBody>
-          <TableRow>
-            {tableStatus ? (
-              <TableCell colSpan={8} className="text-center">
-                {tableStatus}
-              </TableCell>
-            ) : (
-              contacts.map((data, index) => (
-                <React.Fragment key={index}>
-                  <TableCell className="text-center">{data.name}</TableCell>
-                  <TableCell className="text-center">{data.email}</TableCell>
-                  <TableCell className="text-center">{data.phone}</TableCell>
-                  <TableCell className="text-center">{data.wa_phone}</TableCell>
-                  <TableCell
-                    className={cn(
-                      'text-center',
-                      data.status === 1 ? 'text-green-500' : 'text-red-500',
-                    )}
-                  >
-                    {data.status_text}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    <div className="flex justify-center items-center gap-2">
-                      <PencilSquareIcon className="size-5 text-blue-500 hover:text-blue-700 cursor-pointer" />
-                      <button className="text-red-500 hover:text-red-700">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </TableCell>
-                </React.Fragment>
-              ))
-            )}
-          </TableRow>
-        </TableBody>
+        <TableDataUI
+          data={contacts}
+          isFetching={isFetching}
+          isLoading={isLoading}
+          onRetry={onRetry}
+          headers={headers}
+          error={error}
+        />
       </Table>
       <Pagination meta={meta} context="contacts" onPageChange={(page) => onMeta({ page })} />
     </div>
