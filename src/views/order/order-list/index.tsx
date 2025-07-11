795cf426170175b6c80192a8cf4ba14e882e3205
mahardikakdenie <kucingemas744@gmail.com>
2025-07-11 18:09:12 +0700

    added margin button

--- src/views/order/order-list/index.tsx
+++ src/views/order/order-list/index.tsx
@@ -5,7 +5,7 @@ import OrderListTable from './components/Table';
 const OrderListViews: React.FC = () => {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-12">
+      <div className="col-span-12 mb-4">
         <OrderListHeader />
       </div>
       <div className="col-span-12">
