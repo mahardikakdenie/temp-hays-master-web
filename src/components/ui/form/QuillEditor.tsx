bb72a8c1b71c8bdd1a6cf6b9b36fd90a2f38b699
mahardikakdenie <kucingemas744@gmail.com>
2025-07-29 21:37:26 +0700

    fix error class for quill

--- src/components/ui/form/QuillEditor.tsx
+++ src/components/ui/form/QuillEditor.tsx
@@ -76,7 +76,7 @@ const QuillNoSSRWrapper = ({
           id={id}
           className={cn(
             'quill-editor w-full px-4 py-2 border border-gray-600 rounded-t-none rounded-b-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500',
-            error ? 'border-red-700 focus:ring-red-500' : '',
+            error ? 'border-red-700 focus:ring-red-700' : '',
             className,
           )}
         />
