bcb6b26e979fbd83418aa482a087092fe2313db1
mahardikakdenie <kucingemas744@gmail.com>
2025-08-02 01:15:57 +0700

    user heasder refactor to component based

--- src/views/setting/user/components/UserHeader.tsx
+++ src/views/setting/user/components/UserHeader.tsx
@@ -1,34 +1,21 @@
 import type React from 'react';
 import { useGlobal } from '@/contexts/global.context';
-import { usePermission } from '@/contexts/permission.context';
-import Breadcrumbs from '@/components/ui/breadcrumbs/Breadcrumbs';
-import ButtonPrimary from '@/components/ui/button/ButtonPrimary';
-import PlusIcon from '@/components/icons/Plus';
+import PageHeader from '@/components/ui/page/Header';
 
 const UserHeader: React.FC = () => {
   const { onOpenModal } = useGlobal();
-  const { hasPermission } = usePermission();
 
   return (
-    <div className="flex flex-col sm:flex-row justify-between sm:items-end gap-5">
-      <div>
-        <span className="text-xl font-semibold block mb-2">Setup User</span>
-        <Breadcrumbs
-          items={[
-            { title: 'Setting', href: '#' },
-            { title: 'User', href: '#' },
-          ]}
-        />
-      </div>
-
-      {hasPermission('create') && (
-        <div>
-          <ButtonPrimary icon={<PlusIcon className="w-5 h-5" />} onClick={() => onOpenModal('add')}>
-            Add New User
-          </ButtonPrimary>
-        </div>
-      )}
-    </div>
+    <PageHeader
+      title="User List"
+      titleButton="Create new User"
+      isShowBtn
+      items={[
+        { title: 'Setting', href: '#' },
+        { title: 'User', href: '#' },
+      ]}
+      onClick={() => onOpenModal('add')}
+    />
   );
 };
 
