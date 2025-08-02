8f719b5777f21df6fbe8b58163b9df09df49d10e
mahardikakdenie <kucingemas744@gmail.com>
2025-08-02 14:29:48 +0700

    user maintenance useraccess

--- src/views/setting/user/hooks/useUser.hook.ts
+++ src/views/setting/user/hooks/useUser.hook.ts
@@ -1,7 +1,11 @@
 import { useCallback, useState } from 'react';
 import { useGlobal } from '@/contexts/global.context';
-import type { Filter } from '@/types/commons.types';
+import type { Filter, Options } from '@/types/commons.types';
 import dayjs from 'dayjs';
+import { useQuery } from '@tanstack/react-query';
+import { useInternal } from '@/hooks/useInternal';
+import { Routes } from '@/libs/constants/routes.const';
+import { HttpStatus } from '@/libs/constants/httpStatus.const';
 
 const INITIAL_FILTER = {
   startDate: '',
@@ -11,6 +15,7 @@ const INITIAL_FILTER = {
 
 const useUser = () => {
   const { onCloseModal } = useGlobal();
+  const internalApi = useInternal();
 
   const [filter, setFilter] = useState<Filter>(INITIAL_FILTER);
   const [appliedFilter, setAppliedFilter] = useState<Filter>(INITIAL_FILTER);
@@ -44,6 +49,19 @@ const useUser = () => {
     setAppliedFilter(INITIAL_FILTER);
   }, []);
 
+  const { data: accessOptions } = useQuery<Options[], Error>({
+    queryKey: ['user-access-options'],
+    queryFn: async () => {
+      const response = await internalApi(Routes.USER_ACCESS_OPTION);
+      if (response.status !== HttpStatus.OK) {
+        throw new Error('Failed to fetch subcategory options');
+      }
+
+      const { data } = await response.json();
+      return data;
+    },
+  });
+
   return {
     filter,
     appliedFilter,
@@ -52,6 +70,7 @@ const useUser = () => {
     onChangeStatus,
     onSubmitFilter,
     onResetFilter,
+    accessOptions,
   };
 };
 
--- src/views/setting/user/index.tsx
+++ src/views/setting/user/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type React from 'react';
-import type { Options } from '@/types/commons.types';
 import dynamic from 'next/dynamic';
 import UserHeader from './components/UserHeader';
 import UserTable from './components/UserTable';
@@ -10,11 +9,7 @@ import useUser from './hooks/useUser.hook';
 const ModalAddUser = dynamic(() => import('./components/ModalAddUser'), { ssr: false });
 const ModalFilterUser = dynamic(() => import('./components/ModalFilterUser'), { ssr: false });
 
-type UserViewProps = {
-  accessOptions?: Options[];
-};
-
-const UserView: React.FC<UserViewProps> = ({ accessOptions }) => {
+const UserView: React.FC = () => {
   const {
     filter,
     appliedFilter,
@@ -23,6 +18,7 @@ const UserView: React.FC<UserViewProps> = ({ accessOptions }) => {
     onChangeStatus,
     onSubmitFilter,
     onResetFilter,
+    accessOptions,
   } = useUser();
 
   return (
