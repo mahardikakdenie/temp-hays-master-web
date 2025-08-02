79f0bda294b42a3c17c123d6f791a1b6464af360
mahardikakdenie <kucingemas744@gmail.com>
2025-08-02 16:29:39 +0700

    role header

--- src/views/maintaince/role/component/RoleHeader.tsx
+++ src/views/maintaince/role/component/RoleHeader.tsx
@@ -1,6 +1,5 @@
 import type React from 'react';
-import Breadcrumbs from '@/components/ui/breadcrumbs/Breadcrumbs';
-import ButtonPrimary from '@/components/ui/button/ButtonPrimary';
+import PageHeader from '@/components/ui/page/Header';
 
 const UserHeader: React.FC = () => {
   const itemBreadcrumbs = [
@@ -8,16 +7,7 @@ const UserHeader: React.FC = () => {
     { title: 'Role', href: '/maintaince/role' },
   ];
   return (
-    <div className="flex justify-between items-end">
-      <div>
-        <span className="text-xl font-semibold block mb-2">Role</span>
-        <Breadcrumbs items={itemBreadcrumbs} />
-      </div>
-
-      <div>
-        <ButtonPrimary className="w-full">Add New Role</ButtonPrimary>
-      </div>
-    </div>
+    <PageHeader items={itemBreadcrumbs} title="Role" titleButton="Create new role" isShowBtn />
   );
 };
 
